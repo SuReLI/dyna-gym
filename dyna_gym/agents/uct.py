b2d4786e6761cadb6ea5444c194d346ba0d7eb3e
erwanlecarpentier <erwan.lc2@gmail.com>
2018-10-01 18:41:29 +0200

    UCT agent updated

--- dyna_gym/agents/uct.py
+++ dyna_gym/agents/uct.py
@@ -38,7 +38,7 @@ class DecisionNode:
     '''
     Decision node class, labelled by a state
     '''
-    def __init__(self, parent, state, is_terminal):
+    def __init__(self, parent, state, possible_actions, is_terminal):
         self.parent = parent
         self.state = state
         self.is_terminal = is_terminal
@@ -47,6 +47,8 @@ class DecisionNode:
         else: # Non root node
             self.depth = parent.depth + 1
         self.children = []
+        self.possible_actions = possible_actions
+        random.shuffle(self.possible_actions)
         self.explored_children = 0
         self.visits = 0
 
@@ -66,13 +68,13 @@ class UCT(object):
     '''
     UCT agent
     '''
-    def __init__(self, action_space, rollouts, max_depth, gamma=0.9, ucb_constant=6.36396103068, is_model_dynamic=True):
-        self.action_space = action_space
-        self.gamma = gamma
+    def __init__(self, action_space, rollouts, gamma=0.9, ucb_constant=6.36396103068, is_model_dynamic=True):
+        self.action_space = list(combinations(action_space))
+        self.n_actions = len(self.action_space)
         self.rollouts = rollouts
-        self.max_depth = max_depth
-        self.is_model_dynamic = is_model_dynamic
+        self.gamma = gamma
         self.ucb_constant = ucb_constant
+        self.is_model_dynamic = is_model_dynamic
 
     def reset(self):
         '''
@@ -90,7 +92,7 @@ class UCT(object):
         '''
         Compute the entire UCT procedure
         '''
-        self.root = DecisionNode(None, env.state, done)
+        self.root = DecisionNode(None, env.state, self.action_space.copy(), done)
         for _ in range(self.rollouts):
             rewards = [] # Rewards collected along the tree for the current rollout
             node = self.root # Current node
@@ -98,37 +100,65 @@ class UCT(object):
 
             # Selection
             select = True
-            expand_chance_node = False
-            while select and (len(self.root.children) != 0):
-                if (type(node) == DecisionNode): # Decision node
-                    if node.is_terminal: # Terminal, evaluate parent
-                        node = node.parent
-                        select = False
-                    else: # Decision node is not terminal
-                        if node.explored_children < len(node.children): # Go to unexplored chance node
-                            child = node.children[node.explored_children]
-                            node.explored_children += 1
-                            node = child
-                            select = False
+            while select:
+                if (type(node) == DecisionNode): # DecisionNode
+                    if node.is_terminal:
+                        select = False # Selected a terminal DecisionNode
+                    else:
+                        if len(node.children) < self.n_actions:
+                            select = False # Selected a non-fully-expanded DecisionNode
                         else: # Go to chance node maximizing UCB
                             node = max(node.children, key=self.ucb)
                 else: # Chance Node
-                    state_p, reward, terminal = env.transition(node.parent.state,node.action,self.is_model_dynamic)
+                    state_p, reward, terminal = env.transition(node.parent.state, node.action, self.is_model_dynamic)
                     rewards.append(reward)
-                    if (len(node.children) == 0): # No child
-                        expand_chance_node = True
-                        select = False
-                    else: # Already has children
+                    if (len(node.children) == 0):
+                        select = False # Selected a ChanceNode
+                    else:
+                        new_state = True
                         for i in range(len(node.children)):
-                            if env.equality_operator(node.children[i].state,state_p): # State already sampled
+                            if env.equality_operator(node.children[i].state, state_p):
                                 node = node.children[i]
+                                new_state = False
                                 break
-                            else: # New state sampled
-                                expand_chance_node = True
-                                select = False
-                                break
+                        if new_state:
+                            select = False # Selected a ChanceNode
 
             # Expansion
+            if (type(node) == ChanceNode) or ((type(node) == DecisionNode) and not node.is_terminal):
+                if (type(node) == DecisionNode):
+                    node.children.append(ChanceNode(node, node.possible_actions.pop()))
+                    node = node.children[-1]
+                    state_p, reward, terminal = env.transition(node.parent.state ,node.action, self.is_model_dynamic)
+                    rewards.append(reward)
+                # ChanceNode
+                assert(type(node) == ChanceNode) #TODO remove
+                node.children.append(DecisionNode(node, state_p, self.action_space.copy(), terminal))
+                node = node.children[-1]
+
+            # Evaluation
+            assert(type(node) == DecisionNode)
+            t = 0
+            estimate = reward
+            state = node.state
+            while not terminal:
+                action = env.action_space.sample() # default policy
+                state, reward, terminal = env.transition(state, action, self.is_model_dynamic)
+                estimate += reward * (self.gamma**t)
+                t += 1
+
+            # Backpropagation
+            node.visits += 1
+            node = node.parent
+            assert(type(node) == ChanceNode)
+            while node:
+                node.sampled_returns.append(estimate)
+                if len(rewards) != 0:
+                    estimate = rewards.pop() + self.gamma * estimate
+                node.parent.visits += 1
+                node = node.parent.parent
+
+            '''
             if expand_chance_node and (type(node) == ChanceNode): # Expand a chance node
                 node.children.append(DecisionNode(node, state_p, terminal))
                 node = node.children[-1]
@@ -161,4 +191,5 @@ class UCT(object):
                     estimate = rewards.pop() + self.gamma * estimate
                 node.parent.visits += 1
                 node = node.parent.parent
+            '''
         return max(self.root.children, key=chance_node_value).action
