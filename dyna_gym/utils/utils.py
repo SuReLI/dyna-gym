4aa3b79533753e47427b69ada89094efc98f35f8
erwanlecarpentier <erwan.lc2@gmail.com>
2018-10-11 11:42:44 +0200

    Equality comparison between vector's coefficients implemented in libutils

--- dyna_gym/utils/utils.py
+++ dyna_gym/utils/utils.py
@@ -1,4 +1,11 @@
+import numpy as np
 from math import isclose
 
 def close(a, b, r=13):
     return isclose(round(a,r), round(b,r), rel_tol=1e-12, abs_tol=0.0)
+
+def are_coeff_equal(v):
+    return bool(np.prod(list(v[i] == v[i+1] for i in range(len(v)-1)), axis=0))
+
+def are_coeff_close(v):
+    return bool(np.prod(list(close(v[i],v[i+1]) for i in range(len(v)-1)), axis=0))
