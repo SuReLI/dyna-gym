c1aaecc1fec49e071cf410ca759a5d8260cc5635
erwanlecarpentier <erwan.lc2@gmail.com>
2018-11-19 11:16:00 +0100

    Wasserstein Problem solution for exact case and 'integer' metric integrated to main code

--- dyna_gym/utils/distribution.py
+++ dyna_gym/utils/distribution.py
@@ -3,24 +3,24 @@ Helpful functions when dealing with distributions
 """
 
 import numpy as np
-import cvxpy as cp
 import dyna_gym.utils.utils as utl
-from math import sqrt
 from scipy.stats import wasserstein_distance
+from scipy.optimize import linprog
+from math import sqrt
 
 def random_tabular(size):
-    '''
+    """
     Generate a 1D numpy array whose coefficients sum to 1
-    '''
+    """
     w = np.random.random(size)
     return w / np.sum(w)
 
 def random_constrained(u, maxdist):
-    '''
+    """
     Randomly generate a new distribution st the Wasserstein distance between the input
     distribution u and the generated distribution is smaller than the input maxdist.
     Notice that the generated distribution has the same support as the input distribution.
-    '''
+    """
     max_n_trial = int(1e4) # Maximum number of trials
     val = np.asarray(range(len(u)))
     v = random_tabular(val.size)
@@ -32,53 +32,41 @@ def random_constrained(u, maxdist):
     print('Failed to generate constrained distribution after {} trials'.format(max_n_trial))
     exit()
 
-def step_zero_coeff_active(w0, g):
-        l = list((w0[i] / g[i]) for i in range(len(w0)) if (g[i] > 0))#TRM
-        if len(l) == 0:#TRM
-            print('\n\n')
-            print(w0)
-            print(g)
-            print('\n\n')
-        return min((w0[i] / g[i]) for i in range(len(w0)) if (g[i] > 0))
-
-def step_wass_active(c, g):
-    n = len(g)
-    f = cp.Variable(n)
-    A = np.eye(n, dtype=int)
-    A = np.delete(A, n-1, axis=0)
-    for i in range(n-1):
-        A[i, i+1] = -1
-    A = np.concatenate((A,-A), axis=0)
-    objective = cp.Maximize(cp.sum(cp.diag(g) * f))
-    constraints = [A * f <= 1]
-    prob = cp.Problem(objective, constraints)
-    result = prob.solve()
-    return c / result
-
-def clean_almost_zero_coefficients(w):
+def clean_distribution(w):
     for i in range(len(w)):
         if utl.close(w[i], 0.0):
             w[i] = 0.0
-        elif w[i] < 0.0:
-            print('Error: negative weight computed ({}th index): w={}'.format(i, w))
-            exit()
+        else:
+            assert w[i] > 0.0, 'Error: negative weight computed ({}th index): w={}'.format(i, w)
     return w
 
 def worst_dist(v, w0, c):
-    '''
+    """
     Generate argmin_w (w^T v) st W1(w,w0) <= c where W1 is the Wasserstein distance
-    '''
-    if utl.are_coeff_equal(v):
-        return w0
-    else:
-        n = len(v)
-        u = np.ones(shape=n)
-        g = v - (np.dot(u, v) / float(n)) * u
-        g = g / sqrt(np.dot(g, g))
-        alpha = min(
-            step_zero_coeff_active(w0, g),
-            step_wass_active(c, g)
-        )
-        wstar = w0 - alpha * g
-        wstar = clean_almost_zero_coefficients(wstar)
-        return wstar
+    """
+    n = len(v)
+    obj = np.concatenate((v, np.zeros(shape=n)), axis=0)
+
+    U = np.zeros(shape=(n,n))
+    for i in range(n):
+        for j in range(i+1):
+            U[i][j] = 1.0
+    A1 = np.concatenate((-np.eye(n),np.zeros(shape=(n,n))), axis=1)
+    A2 = np.reshape(np.concatenate((np.zeros(n), np.ones(n))), newshape=(1,2*n))
+    A3 = np.concatenate((U,-np.eye(n)), axis=1)
+    A4 = np.concatenate((-U,-np.eye(n)), axis=1)
+    A = np.concatenate((A1,A2,A3,A4), axis=0)
+
+    b1 = np.zeros(n)
+    b2 = np.asarray([c])
+    b3 = np.dot(U,w0)
+    b4 = np.dot(-U,w0)
+    b = np.concatenate((b1,b2,b3,b4), axis=0)
+
+    Ae = np.reshape(np.concatenate((np.ones(n),np.zeros(n))), newshape=(1,2*n))
+    be = np.asarray([1])
+
+    res = linprog(obj, A_eq=Ae, b_eq=be, A_ub=A, b_ub=b)#, method='interior-point')
+
+    x = res.x[:n]
+    return clean_distribution(x)
