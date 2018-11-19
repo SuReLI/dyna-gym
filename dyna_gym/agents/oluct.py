e11b37cb425d813c08e04240f30c827502a26d9f
erwanlecarpentier <erwan.lc2@gmail.com>
2018-11-19 16:52:31 +0100

    OLUCT algorithm reset implemented

--- dyna_gym/agents/oluct.py
+++ dyna_gym/agents/oluct.py
@@ -9,9 +9,9 @@ from math import sqrt, log
 from copy import copy
 
 def value(node):
-    '''
+    """
     Value of the node
-    '''
+    """
     return sum(node.sampled_returns) / len(node.sampled_returns)
 
 def combinations(space):
@@ -35,9 +35,9 @@ class Node:
         self.sampled_returns = []
 
 class OLUCT(object):
-    '''
+    """
     OLUCT agent
-    '''
+    """
     def __init__(self, action_space, gamma=0.9, rollouts=100, max_depth=300, ucb_constant=6.36396103068, is_model_dynamic=True):
         self.action_space = action_space
         self.gamma = gamma
@@ -46,22 +46,44 @@ class OLUCT(object):
         self.ucb_constant = ucb_constant
         self.is_model_dynamic = is_model_dynamic
 
-    def reset(self):
-        '''
-        Reset Agent's attributes.
-        Nothing to reset.
-        '''
+    def display(self):
+        """
+        Display infos about the attributes.
+        """
+        print('Displaying OLUCT agent:')
+        print('Action space       :', self.action_space)
+        print('Number of actions  :', self.n_actions)
+        print('Rollouts           :', self.rollouts)
+        print('Horizon            :', self.horizon)
+        print('Gamma              :', self.gamma)
+        print('UCB constant       :', self.ucb_constant)
+        print('Is model dynamic   :', self.is_model_dynamic)
+
+    def reset(self, p):
+        """
+        Reset the attributes.
+        Expect to receive them in the same order as init.
+        p : list of parameters
+        """
+        assert len(p) == 6, 'Error: expected 6 parameters received {}'.format(len(p))
+        assert type(p[0]) == spaces.discrete.Discrete, 'Error: wrong type, expected "gym.spaces.discrete.Discrete", received {}'.format(type(p[0]))
+        assert type(p[1]) == int, 'Error: wrong type, expected "int", received {}'.format(type(p[1]))
+        assert type(p[2]) == int, 'Error: wrong type, expected "int", received {}'.format(type(p[2]))
+        assert type(p[3]) == float, 'Error: wrong type, expected "float", received {}'.format(type(p[3]))
+        assert type(p[4]) == float, 'Error: wrong type, expected "float", received {}'.format(type(p[4]))
+        assert type(p[5]) == bool, 'Error: wrong type, expected "bool", received {}'.format(type(p[5]))
+        self.__init__(p[0], p[1], p[2], p[3], p[4], p[5])
 
     def ucb(self, node):
-        '''
+        """
         Upper Confidence Bound
-        '''
+        """
         return value(node) + self.ucb_constant * sqrt(log(len(node.parent.sampled_returns))/len(node.sampled_returns))
 
     def act(self, env, done):
-        '''
+        """
         Compute the entire OLUCT procedure
-        '''
+        """
         root = Node(None, None)
         for _ in range(self.rollouts):
             rewards = [] # Rewards collected along the tree for the current rollout
