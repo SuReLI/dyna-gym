be6d6f12c2c93aa5af901a600097f9ae9928a700
erwanlecarpentier <erwan.lc2@gmail.com>
2019-01-14 18:36:01 -0500

    NSCliff rendu plus calme

--- dyna_gym/envs/nscliff_v0.py
+++ dyna_gym/envs/nscliff_v0.py
@@ -197,7 +197,9 @@ class NSCliffV0(Env):
                             sp_down = i
                     w0 = np.array(T[s,a,0,:])
                     wsat = np.zeros(shape=w0.shape)
-                    wsat[sp_down] = 1.0
+                    wsat[sp_norm] = 0.1
+                    wsat[sp_down] = 0.9
+                    #wsat[sp_down] = 1.0
                     D = self.distances_matrix(range(self.nS))
                     l = self.tau * self.L_p / distribution.wass_dual(w0, wsat, D)
                     for t in range(1, self.nT):
