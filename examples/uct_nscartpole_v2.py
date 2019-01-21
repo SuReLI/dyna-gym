9fb2d67cd09e013e9337286c566251e0c3a4b315
erwanlecarpentier <erwan.lc2@gmail.com>
2019-01-21 13:13:27 -0500

    example updated

--- examples/uct_nscartpole_v2.py
+++ examples/uct_nscartpole_v2.py
@@ -6,20 +6,17 @@ import dyna_gym.agents.uct as uct
 env = gym.make('NSCartPole-v2')
 agent = uct.UCT(
     action_space=env.action_space,
-    rollouts=50,
+    rollouts=100,
     horizon=50,
-    is_model_dynamic=True
+    is_model_dynamic=False
 )
 
 ### Run
 env.reset()
 done = False
-verbose = False
 timeout = 1000
 for ts in range(timeout):
     __, reward, done, __ = env.step(agent.act(env,done))
-    if verbose:
-        env.print_state()
     env.render()
     if ts+1 == timeout:
         print("Successfully reached end of episode ({} timesteps)".format(ts+1))
